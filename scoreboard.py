54e21005abd41d013e89a2d88b3bb411ff841f16
Filip Ostrowski <filip.ostrowski96@gmail.com>
2023-07-17 14:10:48 +0200

    4

--- scoreboard.py
+++ scoreboard.py
@@ -6,13 +6,16 @@ FONT = ("Courier", 20, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
         self.high_score = 0
+
+        with open("highscore.txt") as file:
+            self.high_score = int(file.read())
+        self.score = 0
         self.penup()
         self.setposition(x=0, y=270)
         self.hideturtle()
         self.color("white")
-        self.write(f"Score : {self.score}", align=ALIGMENT, font=FONT)
+        self.write(f"Score : {self.score} High Score: {self.high_score}", align=ALIGMENT, font=FONT)
 
     def update_scoreboard(self):
         self.clear()
@@ -25,6 +28,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("highscore.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
